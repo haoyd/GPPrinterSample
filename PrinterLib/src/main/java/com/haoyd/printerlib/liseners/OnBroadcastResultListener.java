a92aa9ec65e84a60f21804b5e1d8d20688fc3add
haoyundong <277079581@qq.com>
2019-05-06 15:29:25 +0800

    删减一些没用的类

--- PrinterLib/src/main/java/com/haoyd/printerlib/liseners/OnBroadcastResultListener.java
+++
@@ -1,9 +0,0 @@
-package com.haoyd.printerlib.liseners;
-
-import com.haoyd.printerlib.entities.CastInfo;
-
-public interface OnBroadcastResultListener {
-
-    void onBroadcastResult(CastInfo result);
-
-}
